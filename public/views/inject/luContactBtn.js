701837aaf59aa361ee75925b340de702f9ba550b
Channing Norton <cnorton9@ur.rochester.edu>
2023-04-04 12:42:25 -0400

    Filestructure change
    Performed at the request of Lyrasis Support

---
+++ public/views/inject/luContactBtn.js
@@ -0,0 +1,8 @@
+function openBtn() {
+    const elem = document.getElementById('contactUsContent');
+    elem.style.display = 'flex';
+    }
+    function closeBtn() {
+    const elem = document.getElementById('contactUsContent');
+    elem.style.display = 'none';
+    } 
