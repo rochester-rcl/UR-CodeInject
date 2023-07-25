5f39fbe1eaa2ba42c020375f048a6db35534b24d
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-07-25 14:54:28 -0400

    Adjusting IF statement

--- public/assets/preservicaLinks.js
+++ public/assets/preservicaLinks.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function(e){
     const header = document.querySelector('#add_desc .panel-body .note .otherfindaid h3');
     
     const boxInfo = document.querySelector('#add_desc .panel-body .note .otherfindaid');
-    console.log('here', header);
-    if (header.innerHTML === 'Preservica Public URL') {
+    console.log('here', header.innerHTML);
+    if (header.innerHTML.toLowerCase().includes('preservica public url')) {
       header.innerHTML = 'Access Digital Version';
     }
-    if (header.innerHTML === 'Preservica Internal URL') {
+    if (header.innerHTML.toLowerCase().includes('preservica internal url')) {
       boxInfo.style.display = 'none';
     }
           });
