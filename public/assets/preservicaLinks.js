ec111a8f173a450d7ac5d4dcb88f4f62b85ae984
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-07-25 13:12:18 -0400

    Changing class name for testing purposes.

--- public/assets/preservicaLinks.js
+++ public/assets/preservicaLinks.js
@@ -1,9 +1,10 @@
 /* Inject Location: body_top */
 
 document.addEventListener("DOMContentLoaded", function(e){
-    const header = document.querySelector('#add_desc .panel-body .note .otherfindaid.single_note h3');
+    const header = document.querySelector('#add_desc .panel-body .note .otherfindaid h3');
     
-    const boxInfo = document.querySelector('#add_desc .panel-body .note .otherfindaid.single_note');
+    const boxInfo = document.querySelector('#add_desc .panel-body .note .otherfindaid');
+    console.log('here', header);
     if (header.innerHTML === 'Preservica Public URL') {
       header.innerHTML = 'Access Digital Version';
     }
