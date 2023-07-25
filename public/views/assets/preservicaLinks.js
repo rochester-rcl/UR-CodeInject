7c7ae39ecc8c56cdeb6df98772e0dec2c83f87e5
Channing Norton <24469058+C-Norton@users.noreply.github.com>
2023-07-25 11:41:11 -0400

    Create preservicaLinks.js

---
+++ public/views/assets/preservicaLinks.js
@@ -0,0 +1,14 @@
+/* Inject Location: body_top */
+
+document.addEventListener("DOMContentLoaded", function(e){
+    const header = document.querySelector('#add_desc .panel-body .note .otherfindaid.single_note h3');
+    
+    const boxInfo = document.querySelector('#add_desc .panel-body .note .otherfindaid.single_note');
+    if (header.innerHTML === 'Preservica Public URL') {
+      header.innerHTML = 'Access Digital Version';
+    }
+    if (header.innerHTML === 'Preservica Internal URL') {
+      boxInfo.style.display = 'none';
+    }
+          });
+    
