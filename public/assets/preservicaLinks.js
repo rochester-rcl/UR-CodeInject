9678b216da0ca9e553a7d6cfe9e689128f1a4aa2
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-07-25 16:15:12 -0400

    Updating event listener for testing purposes

--- public/assets/preservicaLinks.js
+++ public/assets/preservicaLinks.js
@@ -1,9 +1,11 @@
 /* Inject Location: body_top */
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("readystatechange", event => {
     const header = document.querySelector('#add_desc .panel-body .note .otherfindaid h3');
     
     const boxInfo = document.querySelector('#add_desc .panel-body .note .otherfindaid');
+
+    if(event.target.readyState === "complete") {
     console.log('here', header.innerHTML);
     if (header.innerHTML.toLowerCase().includes('preservica public url')) {
       header.innerHTML = 'Access Digital Version';
@@ -11,5 +13,8 @@ document.addEventListener("DOMContentLoaded", function(e){
     if (header.innerHTML.toLowerCase().includes('preservica internal url')) {
       boxInfo.style.display = 'none';
     }
+
+    }
           });
-    
+
+
