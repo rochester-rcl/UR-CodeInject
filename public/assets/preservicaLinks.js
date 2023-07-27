56dca339cbc7b9ad02633d07aff8ccba09aadb05
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-07-27 12:32:46 -0400

    Removing the IF statement

--- public/assets/preservicaLinks.js
+++ public/assets/preservicaLinks.js
@@ -1,20 +1,15 @@
 /* Inject Location: body_top */
 
-document.addEventListener("readystatechange", event => {
-    const header = document.querySelector('#add_desc .panel-body .note .otherfindaid h3');
+document.addEventListener("DOMContentLoaded", function(e){
+    const header = document.querySelectorAll('#add_desc .panel-body .note .otherfindaid h3');
+    const boxInfo = document.querySelectorAll('#add_desc .panel-body .note .otherfindaid');
+    const internalHeader = document.querySelector('#res_accordion .panel.panel-default #add_desc .panel-body .note .otherfindaid.single_note h3');
     
-    const boxInfo = document.querySelector('#add_desc .panel-body .note .otherfindaid');
 
-    if(event.target.readyState === "complete") {
-    console.log('here', header.innerHTML);
-    if (header.innerHTML.toLowerCase().includes('preservica public url')) {
-      header.innerHTML = 'Access Digital Version';
-    }
-    if (header.innerHTML.toLowerCase().includes('preservica internal url')) {
-      boxInfo.style.display = 'none';
-    }
-
-    }
-          });
+    header[1].innerHTML = 'Access Digital Version';
+    internalHeader.innerHTML = 'Access Digital Version';
+    boxInfo[0].style.display = 'none';
+    `
+      });
 
 
