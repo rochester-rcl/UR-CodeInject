bd12961db25ee366b6a7b761374744109a7fc708
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-08-01 11:50:47 -0400

    Reusing the event listener for testing purposes

--- public/assets/preservicaLinks.js
+++ public/assets/preservicaLinks.js
@@ -1,6 +1,5 @@
 /* Inject Location: body_top */
-
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("readystatechange", event => {
     const header = document.querySelectorAll('#add_desc .panel-body .note .otherfindaid h3');
     const boxInfo = document.querySelectorAll('#add_desc .panel-body .note .otherfindaid');
     const internalHeader = document.querySelector('#res_accordion .panel.panel-default #add_desc .panel-body .note .otherfindaid.single_note h3');
@@ -9,4 +8,4 @@ document.addEventListener("DOMContentLoaded", function(e){
     header[1].innerHTML = 'Access Digital Version';
     internalHeader.innerHTML = 'Access Digital Version';
     boxInfo[0].style.display = 'none';
-      });
+          });
