cddc18538d821db252b357053402753bd770cdfc
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-08-28 10:57:58 -0400

    Update Robbins library name

--- public/views/inject/luNavBarFix.js
+++ public/views/inject/luNavBarFix.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(e){
             <a href="https://www.library.rochester.edu/rbscp">Rare Books and Special Collections</a>
             <a href="https://www.esm.rochester.edu/sibley/">Sibley Music Library</a>
             <a href="https://www.urmc.rochester.edu/libraries/miner.aspx">Edward G. Miner Library History of Medicine Section</a>
-            <a href="https://www.library.rochester.edu/spaces/robbins">Robbins Library</a>
+            <a href="https://www.library.rochester.edu/spaces/robbins">The Rossell Hope Robbins Library </a>
             <a href="https://mag.rochester.edu/">Memorial Art Gallery</a>
     
         </div>
