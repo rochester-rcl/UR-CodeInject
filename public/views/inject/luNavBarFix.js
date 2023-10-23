4ebb58b445006f0ea778133d5f9be21f732adc27
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-10-23 08:45:22 -0400

    Updating Miner's URL

--- public/views/inject/luNavBarFix.js
+++ public/views/inject/luNavBarFix.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         <div class="container">
             <a href="https://www.library.rochester.edu/rbscp">Rare Books and Special Collections</a>
             <a href="https://www.esm.rochester.edu/sibley/">Sibley Music Library</a>
-            <a href="https://www.urmc.rochester.edu/libraries/miner.aspx">Edward G. Miner Library History of Medicine Section</a>
+            <a href="https://www.urmc.rochester.edu/libraries/miner/rare-books-and-manuscripts.aspx">Edward G. Miner Library History of Medicine Section</a>
             <a href="https://www.library.rochester.edu/spaces/robbins">The Rossell Hope Robbins Library </a>
             <a href="https://mag.rochester.edu/">Memorial Art Gallery</a>
     
