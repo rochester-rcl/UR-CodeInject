1343ac4a85e97f4a0e6fa09f2c8796c01c45fd09
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-11-14 15:31:22 -0500

    Adding toggle function

--- public/views/inject/luContactBtn.js
+++ public/views/inject/luContactBtn.js
@@ -8,3 +8,15 @@ function openBtn() {
     const elem = document.getElementById('contactUsContent');
     elem.style.display = 'none';
     } 
+
+function toggleBtn() {
+  var btn1 = document.getElementsByClassName("openBtn");
+  var btn2 = document.getElementsByClassName("quickSurvey");
+    if (btn1[0].style.display === "none" || btn2[0].style.display === 'none') {
+    btn1[0].style.display = "block";
+    btn2[0].style.display = "block";
+  } else {
+    btn1[0].style.display = "none";
+    btn2[0].style.display = "none";
+  }
+}
