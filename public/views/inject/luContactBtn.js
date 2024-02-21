5643506fb9016f173982419216a8f875bd627da6
lvu3 <107505679+lvu3@users.noreply.github.com>
2024-02-21 11:44:46 -0500

    Added new contact button to the blue navbar and remove the old ones

--- public/views/inject/luContactBtn.js
+++ public/views/inject/luContactBtn.js
@@ -1,22 +1,42 @@
 /* Inject Location: body_top */
+document.addEventListener("DOMContentLoaded", function (e) {
+  const blueNavbar = document.querySelector(
+    "#collapsemenu .nav.nav.navbar-nav"
+  );
+  const contactUsBtn = document.createElement("li");
+  contactUsBtn.innerHTML = `
+      <div class="dropdown" data-dropdown>
+      <button class="link" data-dropdown-button>Contact Us</button>
+      <div class="dropdown-menu information-grid">
+          <div id="dropdown-links">
+            <a href="https://www.library.rochester.edu/services/forms/contact-rbscp%22 target="_blank">Rare Books and Special Collections</a>
+            <a href="https://www.esm.rochester.edu/sibley/contact/%22 target="_blank">Sibley Music Library</a>
 
-function openBtn() {
-    const elem = document.getElementById('contactUsContent');
-    elem.style.display = 'flex';
-    }
-    function closeBtn() {
-    const elem = document.getElementById('contactUsContent');
-    elem.style.display = 'none';
-    } 
+            <a href="https://urmc-rochester.libanswers.com/form?queue_id=6183" target="_blank">Edward G.Miner Library</a>
 
-function toggleBtn() {
-  var btn1 = document.getElementsByClassName("openBtn");
-  var btn2 = document.getElementsByClassName("quickSurvey");
-    if (btn1[0].style.display === "none" || btn2[0].style.display === 'none') {
-    btn1[0].style.display = "block";
-    btn2[0].style.display = "block";
-  } else {
-    btn1[0].style.display = "none";
-    btn2[0].style.display = "none";
+            <a href="https://www.library.rochester.edu/about/staff/dept#38%22 target="_blank">Rossell Hope Robbins Library</a>
+
+            <a href="https://mag.rochester.edu/about/contact-us/%22 target="_blank">Memorial Art Gallery</a>
+          </div>
+      </div>
+    </div>
+  `;
+  blueNavbar.appendChild(contactUsBtn);
+});
+
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
   }
-}
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
+
