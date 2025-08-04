0e6f57caa01080c49ac3d5609bf641541187d8bb
lvu3 <107505679+lvu3@users.noreply.github.com>
2025-08-04 10:00:23 -0400

    removing the search button

--- public/views/inject/luNavBarFix.js
+++ public/views/inject/luNavBarFix.js
@@ -24,15 +24,15 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 
 /* Adding text to the search button */
-document.addEventListener("DOMContentLoaded", function(e){
-    let searchBtn = document.querySelector("#navigation .navbar .collapse .navbar-nav li:nth-child(8)");
-    searchBtn.innerHTML = ` 
-      <a href="/search?reset=true" title="Search The Archives">
-                <span class="fa fa-search" aria-hidden="true"></span>
-                 Search<span class="sr-only">Search The Archives</span>
-              </a>
-    `
-      });
+// document.addEventListener("DOMContentLoaded", function(e){
+//     let searchBtn = document.querySelector("#navigation .navbar .collapse .navbar-nav li:nth-child(8)");
+//     searchBtn.innerHTML = ` 
+//       <a href="/search?reset=true" title="Search The Archives">
+//                 <span class="fa fa-search" aria-hidden="true"></span>
+//                  Search<span class="sr-only">Search The Archives</span>
+//               </a>
+//     `
+//       });
 
 /* Adjusting placeholder in search bar */
 document.addEventListener("DOMContentLoaded", function(e){
@@ -68,3 +68,4 @@ document.addEventListener("DOMContentLoaded", function(e){
     `;
     btnList.prepend(newPrintBtn);
 });
+
